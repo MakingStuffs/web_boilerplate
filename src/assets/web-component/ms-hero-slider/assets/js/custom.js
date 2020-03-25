ce9dc0be10cee7732ef79b88e1232a64f32e2642
Making Stuffs <33229318+MakingStuffs@users.noreply.github.com>
2020-03-25 13:41:50 +0530

    Removed old social links from web component's custom.js file

--- src/assets/web-component/ms-hero-slider/assets/js/custom.js
+++ src/assets/web-component/ms-hero-slider/assets/js/custom.js
@@ -12,13 +12,13 @@ export const custom = (context) => {
     for(let i = 0; i < logos.length; i++) {
         const a = document.createElement('a');
         if(logos[i].iconName.includes('facebook')) {
-            a.setAttribute('href', 'https://www.facebook.com/La-Ares-693667351011043');
-            a.setAttribute('title', 'La Ares on Facebook');
+            a.setAttribute('href', 'https://facebook.com');
+            a.setAttribute('title', 'Some facebook account');
             a.setAttribute('rel', 'noreferrer noopener');
         }
         if(logos[i].iconName.includes('instagram')){
-            a.setAttribute('href', 'https://www.instagram.com/ares.la');
-            a.setAttribute('title', 'La Ares on Instagram');
+            a.setAttribute('href', 'https://www.instagram.com');
+            a.setAttribute('title', 'Some instagram account');
             a.setAttribute('rel', 'noreferrer noopener');
         }
         a.appendChild(logos[i].node[0]);
